e7b9874f927b143736966edb609033ab5e348cf2
Alice <34670205+AliceCSY@users.noreply.github.com>
2017-12-24 20:35:48 -0500

    Revised comments

--- OthelloGUI.java
+++ OthelloGUI.java
@@ -8,7 +8,7 @@ import java.awt.*;
 import java.io.*;
 
 public class OthelloGUI {
-	// the name of the configuration file
+   // the name of the configuration file
    private final String CONFIGFILE = "images/config.txt";
 	
    private JLabel[][] slots;
@@ -22,36 +22,26 @@ public class OthelloGUI {
    private String logoIcon;
    private String[] iconFile;
 
-/**
-* Number of players
-*/
+   // Number of players
    public final int NUMPLAYER = 2;
 
-/**
-* Number of rows on the game board
-*/
+   // Number of rows on the game board
    public final int NUMROW = 8;
 
-/**
-* Number of colums on the game board
-*/
+   // Number of columns on the game board
    public final int NUMCOL = 8;
 
-/**
-* Number of games needed to be won to win the match
-*/
+   // Number of games needed to be won to win the match
    public int MAXGAME;
 	
-/**
-* Number of games needed to be won to win the match
-*/
+   // identification for each player
    public int PLAYER1 = 0;
    public int PLAYER2 = 1;
 
-/**
-* Constants defining the demensions of the different components
-* on the GUI
-*/ 
+   /**
+   * Constants defining the demensions of the different components
+   * on the GUI.
+   */ 
    private final int PIECESIZE = 70;
    private final int PLAYPANEWIDTH = NUMCOL * PIECESIZE;
    private final int PLAYPANEHEIGHT = NUMROW * PIECESIZE;
@@ -65,11 +55,11 @@ public class OthelloGUI {
    private final int FRAMEWIDTH = (int)(LOGOWIDTH * 1.03);
    private final int FRAMEHEIGHT = (int)((LOGOHEIGHT + PLAYPANEHEIGHT) * 1.1);
 
-// Constructor:  OthelloGUI
-// - intialize variables from config files
-// - initialize the imageIcon array
-// - initialize the slots array
-// - create the main frame
+   // Constructor: OthelloGUI
+   // - intialize variables from config files
+   // - initialize the imageIcon array
+   // - initialize the slots array
+   // - create the main frame
    public OthelloGUI () {
       initConfig();
       initImageIcon();
@@ -77,13 +67,9 @@ public class OthelloGUI {
       createMainFrame();
    }
 
-   private void initConfig() {
-   /* TO DO:  initialize the following variables with information read from the config file
-   *         - MAXGAME
-   *         - logoIcon
-   *         - iconFile
-      */
-      //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   // Initializes MAXGAME, logoIcon, and iconFile with the information read from
+   // the config.txt file.
+   private void initConfig() {       
       try {
          BufferedReader in = new BufferedReader(new FileReader(CONFIGFILE));
          MAXGAME = Integer.parseInt(in.readLine());
@@ -99,8 +85,8 @@ public class OthelloGUI {
       }
    }
 
-// initImageIcon
-// Initialize playerIcon arrays with graphic files
+   // initImageIcon
+   // Initialize playerIcon arrays with graphic files
    private void initImageIcon() {
       playerIcon = new ImageIcon[NUMPLAYER];
       for (int i = 0; i < NUMPLAYER; i++) {
@@ -108,14 +94,14 @@ public class OthelloGUI {
       }
    }
 
-// initSlots
-// initialize the array of JLabels
+   // initSlots
+   // initialize the array of JLabels
    private void initSlots() {
       slots = new JLabel[NUMROW][NUMCOL];
       for (int i = 0; i < NUMROW; i++) {
          for (int j = 0; j < NUMCOL; j++) {
             slots [i] [j] = new JLabel ();
-         // slots[i][j].setFont(new Font("SansSerif", Font.BOLD, 18));
+            // slots[i][j].setFont(new Font("SansSerif", Font.BOLD, 18));
             slots[i][j].setPreferredSize(new Dimension(PIECESIZE, PIECESIZE));
             slots [i] [j].setHorizontalAlignment (SwingConstants.CENTER);
             slots [i] [j].setBorder (new LineBorder (Color.white));        
@@ -123,7 +109,7 @@ public class OthelloGUI {
       }
    }
 
-// createPlayPanel
+   // createPlayPanel
    private JPanel createPlayPanel() {
       JPanel panel = new JPanel(); 
       panel.setPreferredSize(new Dimension(PLAYPANEWIDTH, PLAYPANEHEIGHT));
@@ -137,7 +123,7 @@ public class OthelloGUI {
       return panel;    
    }
 
-// createInfoPanel
+   // createInfoPanel
    private JPanel createInfoPanel() {
    
       JPanel panel = new JPanel();
@@ -148,23 +134,23 @@ public class OthelloGUI {
       Font headingFont = new Font ("Calibri", Font.BOLD, 20);
       Font regularFont = new Font ("Serif", Font.BOLD, 16);
    
-   // Create a panel for the scoreboard
+      // Create a panel for the scoreboard
       JPanel scorePanel = new JPanel();
       scorePanel.setBackground(background);
    
-   // Create the label to display "SCOREBOARD" heading
+      // Create the label to display "SCOREBOARD" heading
       JLabel scoreLabel = new JLabel ("SCOREBOARD", JLabel.CENTER);
       scoreLabel.setFont(headingFont);
       scoreLabel.setAlignmentX (Component.CENTER_ALIGNMENT);
-   //scoreLabel.setForeground(Color.white);
+      //scoreLabel.setForeground(Color.white);
    
-   // Create JLabels for players
+      // Create JLabels for players
       JLabel[] playerLabel = new JLabel[NUMPLAYER];
       for (int i = 0; i < NUMPLAYER; i++) {
          playerLabel[i] = new JLabel(playerIcon[i]);
       }
    
-   // Create the array of textfield for players' score
+      // Create the array of textfield for players' score
       playerScore = new JTextField[NUMPLAYER];
       for (int i = 0; i < NUMPLAYER; i++) {
          playerScore[i] = new JTextField();
@@ -185,13 +171,13 @@ public class OthelloGUI {
       JPanel nextPanel = new JPanel();
       nextPanel.setBackground(background);
    
-   // Create the label to display "NEXT TURN" heading
+      // Create the label to display "NEXT TURN" heading
       JLabel nextLabel = new JLabel ("NEXT TURN", JLabel.CENTER);
       nextLabel.setFont(headingFont);
       nextLabel.setAlignmentX (Component.CENTER_ALIGNMENT);
-   //nextLabel.setForeground(Color.white);
+      // nextLabel.setForeground(Color.white);
    
-   // Create the JLabel for the nextPlayer
+      // Create the JLabel for the nextPlayer
       nextPlayerIcon = new JLabel();
       System.out.println(nextPlayerIcon.getAlignmentX());
       nextPlayerIcon.setAlignmentX(JLabel.CENTER_ALIGNMENT);
@@ -206,44 +192,44 @@ public class OthelloGUI {
       return panel;
    }
 
-// createMainFrame
+   // createMainFrame
    private void createMainFrame() {
    
-   // Create the main Frame
+      // Create the main Frame
       mainFrame = new JFrame ("Othello");
       JPanel panel = (JPanel)mainFrame.getContentPane();
       panel.setLayout (new BoxLayout(panel,BoxLayout.Y_AXIS));
    
-   // Create the panel for the logo
+      // Create the panel for the logo
       JPanel logoPane = new JPanel();
       logoPane.setPreferredSize(new Dimension (LOGOWIDTH, LOGOHEIGHT));
       JLabel logo = new JLabel();
       logo.setIcon(new ImageIcon(logoIcon));
       logoPane.add(logo);
    
-   // Create the bottom Panel which contains the play panel and info Panel
+      // Create the bottom Panel which contains the play panel and info Panel
       JPanel bottomPane = new JPanel();
       bottomPane.setLayout(new BoxLayout(bottomPane,BoxLayout.X_AXIS));
       bottomPane.setPreferredSize(new Dimension(PLAYPANEWIDTH + INFOPANEWIDTH, PLAYPANEHEIGHT));
       bottomPane.add(createPlayPanel());
       bottomPane.add(createInfoPanel());
    
-   // Add the logo and bottom panel to the main frame
+      // Add the logo and bottom panel to the main frame
       panel.add(logoPane);
       panel.add(bottomPane);
    
       mainFrame.setContentPane(panel);
-   //   mainFrame.setPreferredSize(new Dimension(FRAMEWIDTH, FRAMEHEIGHT));
+      // mainFrame.setPreferredSize(new Dimension(FRAMEWIDTH, FRAMEHEIGHT));
       mainFrame.setSize(FRAMEWIDTH, FRAMEHEIGHT);
       mainFrame.setVisible(true);
    }
 
-/**
-* Returns the column number of where the given JLabel is on
-* 
-* @param  label the label whose column number to be requested
-* @return the column number
-*/
+   /**
+   * Returns the column number of where the given JLabel is on
+   * 
+   * @param  label the label whose column number to be requested
+   * @return the column number
+   */
    public int getRow(JLabel label) {
       int result = -1;
       for (int i = 0; i < NUMROW && result == -1; i++) {
@@ -256,12 +242,12 @@ public class OthelloGUI {
       return result;
    }
 
-/**
-* Returns the column number of where the given JLabel is on
-* 
-* @param  label the label whose column number to be requested
-* @return the column number
-*/
+   /**
+   * Returns the column number of where the given JLabel is on
+   * 
+   * @param  label the label whose column number to be requested
+   * @return the column number
+   */
    public int getColumn(JLabel label) {
       int result = -1;
       for (int i = 0; i < NUMROW && result == -1; i++) {
@@ -282,40 +268,39 @@ public class OthelloGUI {
       }
    }
 
-/**
-* Display the specified player icon on the specified slot
-* 
-* @param row row of the slot
-* @param col column of the slot
-* @param player player to be displayed
-*/
+   /**
+   * Display the specified player icon on the specified slot
+   * 
+   * @param row row of the slot
+   * @param col column of the slot
+   * @param player player to be displayed
+   */
    public void setPiece(int row, int col, int player) {
       slots[row][col].setIcon(playerIcon[player]);
    }
 
-/**
-* Display the score on the textfield of the corresponding player
-* 
-* @param player the player whose score to be displayed
-* @param score the score to be displayed
-*/
+   /**
+   * Display the score on the textfield of the corresponding player
+   * 
+   * @param player the player whose score to be displayed
+   * @param score the score to be displayed
+   */
    public void setPlayerScore(int player, int score) {
       playerScore[player].setText(score+"");
    }
 
-/**
-* Display the appropriate player icon under"Next Turn"
-* 
-* @param player the player number of the next player; its corresponding icon will be displayed under "Next Turn"
-*/
+   /**
+   * Display the appropriate player icon under"Next Turn"
+   * 
+   * @param player the player number of the next player; its corresponding icon will be displayed under "Next Turn"
+   */
    public void setNextPlayer(int player) {
       nextPlayerIcon.setIcon(playerIcon[player]);
    }
 
-/**
-* Reset the game board (clear all the pieces on the game board)
-* 
-*/
+   /**
+   * Reset the game board (clear all the pieces on the game board)
+   */
    public void resetGameBoard() {
       for (int i = 0; i < NUMROW; i++) {
          for (int j = 0; j < NUMCOL; j++) {
@@ -324,52 +309,53 @@ public class OthelloGUI {
       }
    }
 
-/**
-* Display a pop up window displaying the message about invalid move
-* 
-*/
+   /**
+   * Display a pop up window displaying the message about invalid move
+   */
    public void showInvalidMoveMessage(){
       JOptionPane.showMessageDialog(null, " This move is invalid", "Invalid Move", JOptionPane.PLAIN_MESSAGE, null); 
    }
 
-/**
-* Display a pop up window specifying the number of opponents that was outflanked
-* 
-* @param player the player number who has outflanked opponents
-* @param outflank the number of opponents that were outflanked
-*/
+   /**
+   * Display a pop up window specifying the number of opponents that was outflanked
+   * 
+   * @param player the player number who has outflanked opponents
+   * @param outflank the number of opponents that were outflanked
+   */
    public void showOutflankMessage(int player, int outflank){
       JOptionPane.showMessageDialog(null, " outflanked " + outflank + " opponent(s).", "OutFlank!", JOptionPane.PLAIN_MESSAGE, playerIcon[player]); 
    }
    
-/**
-* Display a pop up window displaying the message about a tie game
-* 
-*/
+   /**
+   * Display a pop up window displaying the message about a tie game
+   */
    public void showTieGameMessage(){
       JOptionPane.showMessageDialog(null, " This game is tie.", "Tie Game", JOptionPane.PLAIN_MESSAGE, null); 
    }
 
-/**
-* Display a pop up window specifying the winner of this game
-* 
-* @param player the player number of the winner of the game
-*/
+   /**
+   * Display a pop up window specifying the winner of this game
+   * 
+   * @param player the player number of the winner of the game
+   */
    public void showWinnerMessage(int player){
       JOptionPane.showMessageDialog(null, " won this game!", "This game has a winner!", JOptionPane.PLAIN_MESSAGE, playerIcon[player]); 
    }
 
-/**
-* Display a pop up window specifying the winner of the match
-* 
-* @param player the player number of the winner of the match
-*/
+   /**
+   * Display a pop up window specifying the winner of the match
+   * 
+   * @param player the player number of the winner of the match
+   */
    public void showFinalWinnerMessage(int player){
       JOptionPane.showMessageDialog(null, " won the match with " + MAXGAME + " wins", "The match is finished", JOptionPane.PLAIN_MESSAGE, playerIcon[player]); 
       System.exit (0);
    }
-   
-   //i don't know if this is going to work but let's try it!
+	
+   /**
+   * Display a dialog box that allows user to choose between single-player and
+   * two-player game modes.
+   */
    public int setGameMode() {
       String[] gameModes = {"Single Player", "Two Players"};
       int userChoice;
